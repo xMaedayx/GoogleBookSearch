084fc731d9aac579ce39eb1bd00e07ea67c3e800
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-05-24 12:18:51 -0700

    SearchBook usemutation

--- farenheit451/client/src/App.js
+++ farenheit451/client/src/App.js
@@ -3,13 +3,22 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
-function App() {
+const client = new ApolloClient({
+  uri: 'http://your-apollo-server-endpoint',
+  cache: new InMemoryCache(),
+});
+
   return (
     <Router>
       <>
         <Navbar />
         <Switch>
+        <ApolloProvider client={client}>
+            
+        </ApolloProvider>
+
           <Route exact path='/' component={SearchBooks} />
           <Route exact path='/saved' component={SavedBooks} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
--- farenheit451/client/src/pages/SearchBooks.js
+++ farenheit451/client/src/pages/SearchBooks.js
@@ -11,6 +11,11 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { gql, useMutation } from '@apollo/client'; 
+import { SAVE_BOOK } from './mutations';
+
+
+
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -60,6 +65,7 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+  const [saveBookMutation, { error }] = useMutation(SAVE_BOOK);
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
@@ -72,14 +78,15 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { data } = await saveBookMutation({
+        variables: { book: bookToSave },
+      });
+  
+      if (error) {
+        throw new Error('Something went wrong!');
       }
-
-      // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+  
+      setSavedBookIds([...savedBookIds, data.saveBook.savedBooks[data.saveBook.savedBooks.length - 1].bookId]);
     } catch (err) {
       console.error(err);
     }
